88595e8a38cba2699c5970a57daa9dc4d250bb54
GitProjectAS <github7156@gmail.com>
2026-08-19 09:51:46 +1200

    Quit button polishing

--- dot-escape---godot-/scripts/PauseScene.gd
+++ dot-escape---godot-/scripts/PauseScene.gd
@@ -20,3 +20,7 @@ func _on_resume_button_pressed() -> void:
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/MainScreen.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
