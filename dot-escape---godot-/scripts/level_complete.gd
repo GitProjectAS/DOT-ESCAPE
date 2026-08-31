9f94e22a2287b9381e47c6cdcf852149b926b277
GitProjectAS <github7156@gmail.com>
2026-09-01 10:54:51 +1200

    Level complete design in the godot

--- dot-escape---godot-/scripts/level_complete.gd
+++ dot-escape---godot-/scripts/level_complete.gd
@@ -18,3 +18,7 @@ func _on_menu_button_pressed() -> void:
 
 func _on_retry_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+
+
+func _on_next_level_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level2.tscn")
